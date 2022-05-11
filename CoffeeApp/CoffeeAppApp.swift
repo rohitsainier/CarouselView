137b36553865e60cc691f4bb7eba72fb81623bb4
rohitsainier <rohitsainier@gmail.com>
2022-05-11 22:44:15 +0530

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Rohit Saini on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
